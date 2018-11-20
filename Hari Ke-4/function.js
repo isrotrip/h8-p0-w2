16a1e4ca58071fdc84822095caf35fe7a31998ba
isrotrip <isrotrip93@gmail.com>
2018-11-20 20:18:28 +0700

    p0w2d2 20:18

---
+++ Hari Ke-4/function.js
@@ -0,0 +1,41 @@
+//TUGAS 1
+
+ /*Kode Function
+ function shoutOut(){
+    return 'Halo Function!';
+ }
+ */
+
+console.log(shoutOut()) // Menampilkan "Halo Function!" di console
+
+//TUGAS 2
+
+ /*
+ Kode Function
+ function calculateMultiply(a, b){
+    return a * b
+    }
+ */
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian); // Menampilkan angka 30
+
+// TUGAS 3
+
+/*
+ Kode Function
+ function processSentence(nama, umur, alamat, hobi){
+    return 'Nama saya ' + nama + ', umur saya ' + umur + 'tahun, alamat saya di ' + alamat + ', dan saya punya hobby yaitu ' + hobi + '!';
+ }
+*/
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
